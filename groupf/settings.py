d7041c30f22ed0853ff2577bef9daaf38eea3879
oferel <oferel1@gmail>
2015-12-11 00:54:51 +0200

    20

--- groupf/settings.py
+++ groupf/settings.py
@@ -87,8 +87,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = 'groupf/static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
